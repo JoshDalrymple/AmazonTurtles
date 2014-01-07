5e3c284db80be8e922db83a9475f5dbd5e4131ce
= <josh.dalrymple@careerbuilder.com>
2014-01-07 13:23:09 -0500

    Quick CSS Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 AmazonTurtles::Application.routes.draw do
-  get "users/new"
-  get "static_pages/home"
-  get "turtles/new"
+	root  'static_pages#home'
+  	get "users/new"
+  	get "static_pages/home"
+  	get "turtles/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
