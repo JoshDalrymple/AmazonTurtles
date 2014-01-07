3d75ae11bfa7f3fef34e1af87a117c90d9000236
= <josh.dalrymple@careerbuilder.com>
2014-01-07 12:33:17 -0500

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AmazonTurtles::Application.routes.draw do
+  get "users/new"
+  get "static_pages/home"
   get "turtles/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
